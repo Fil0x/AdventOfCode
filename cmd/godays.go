65fa0116e54e19ba945c61c8f6f9d916ce6cabe9
Fadi Samisti <fadisamisti@gmail.com>
2018-12-02 18:11:16 +0100

    Day 2

--- cmd/godays.go
+++ cmd/godays.go
@@ -11,25 +11,26 @@ import (
 var funcMap = map[string]interface{}{
 	"1.1": Day1part1,
 	"1.2": Day1part2,
+	"2.1": Day2part1,
+	"2.2": Day2part2,
 }
 
 func main() {
 	lines := filereader.ReadFile(os.Args[2])
-	res := funcMap[os.Args[1]].(func([]string) int)(lines)
-	fmt.Println(res)
+	resInt, resStr := funcMap[os.Args[1]].(func([]string) (int, string))(lines)
+	fmt.Println(resInt, resStr)
 }
 
-// day1part1 outputs correct 459
-func Day1part1(lines []string) int {
+func Day1part1(lines []string) (int, string) {
 	sum := 0
 	for _, freq := range lines {
 		num, _ := strconv.Atoi(freq)
 		sum += num
 	}
-	return sum
+	return sum, ""
 }
 
-func Day1part2(lines []string) int {
+func Day1part2(lines []string) (int, string) {
 	seen := make(map[int]bool)
 	sum := 0
 	seen[sum] = true
@@ -41,5 +42,64 @@ func Day1part2(lines []string) int {
 		}
 		seen[sum] = true
 	}
-	return sum
+	return sum, ""
+}
+
+func Day2part1(lines []string) (int, string) {
+	twoCount, threeCount := 0, 0
+	for _, boxids := range lines {
+		charMap := make(map[byte]int)
+		candidates := make([]byte, 0)
+		n := len(boxids)
+		for i := 0; i < n; i++ {
+			if _, ok := charMap[boxids[i]]; ok {
+				charMap[boxids[i]]++
+				candidates = append(candidates, boxids[i])
+			} else {
+				charMap[boxids[i]] = 1
+			}
+		}
+		// loop through the candidates and find one exactly 2 and 3
+		twoFound, threeFound := false, false
+		for _, candidate := range candidates {
+			if !twoFound && charMap[candidate] == 2 {
+				twoCount++
+				twoFound = true
+			}
+			if !threeFound && charMap[candidate] == 3 {
+				threeCount++
+				threeFound = true
+			}
+			if twoFound && threeFound {
+				break
+			}
+		}
+	}
+
+	return twoCount * threeCount, ""
+}
+
+func Day2part2(lines []string) (int, string) {
+	for i := 0; i < len(lines)-1; i++ {
+		for j := i + 1; j < len(lines); j++ {
+			differences, pos := diffStrings(lines[i], lines[j])
+			if differences == 1 {
+				return 0, (lines[i][:pos[0]] + lines[i][pos[0]+1:])
+			}
+		}
+	}
+	return 0, ""
+}
+
+func diffStrings(str1 string, str2 string) (int, []int) {
+	counter := 0
+	diffPositions := make([]int, 0)
+	// str1 and str2 are same length
+	for i := 0; i < len(str1); i++ {
+		if str1[i] != str2[i] {
+			counter++
+			diffPositions = append(diffPositions, i)
+		}
+	}
+	return counter, diffPositions
 }
--- cmd/godays_test.go
+++ cmd/godays_test.go
@@ -4,73 +4,89 @@ import "testing"
 
 func TestDay1Part1Simple1(t *testing.T) {
 	lines := []string{"+1", "-2", "+3", "+1"}
-	result := Day1part1(lines)
+	rI, _ := Day1part1(lines)
 	var expected = 3
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part1Simple2(t *testing.T) {
 	lines := []string{"+1", "+1", "+1"}
-	result := Day1part1(lines)
+	rI, _ := Day1part1(lines)
 	var expected = 3
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part1Simple3(t *testing.T) {
 	lines := []string{"+1", "+1", "-2"}
-	result := Day1part1(lines)
+	rI, _ := Day1part1(lines)
 	var expected = 0
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part1Simple4(t *testing.T) {
 	lines := []string{"-1", "-2", "-3"}
-	result := Day1part1(lines)
+	rI, _ := Day1part1(lines)
 	var expected = -6
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part2Simple1(t *testing.T) {
 	lines := []string{"+1", "-2", "+3", "+1"}
-	result := Day1part2(lines)
+	rI, _ := Day1part2(lines)
 	var expected = 2
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part2Simple2(t *testing.T) {
 	lines := []string{"+1", "-1"}
-	result := Day1part2(lines)
+	rI, _ := Day1part2(lines)
 	var expected = 0
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part2Simple3(t *testing.T) {
 	lines := []string{"+3", "+3", "+4", "-2", "-4"}
-	result := Day1part2(lines)
+	rI, _ := Day1part2(lines)
 	var expected = 10
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part2Simple4(t *testing.T) {
 	lines := []string{"-6", "+3", "+8", "+5", "-6"}
-	result := Day1part2(lines)
+	rI, _ := Day1part2(lines)
 	var expected = 5
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
 	}
 }
 func TestDay1Part2Simple5(t *testing.T) {
 	lines := []string{"+7", "+7", "-2", "-7", "-4"}
-	result := Day1part2(lines)
+	rI, _ := Day1part2(lines)
 	var expected = 14
-	if result != expected {
-		t.Fatalf("Expected %d, got %d", expected, result)
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
+	}
+}
+func TestDay2Part1Simple(t *testing.T) {
+	lines := []string{"abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab"}
+	rI, _ := Day2part1(lines)
+	var expected = 12
+	if rI != expected {
+		t.Fatalf("Expected %d, got %d", expected, rI)
+	}
+}
+func TestDay2Part2Simple(t *testing.T) {
+	lines := []string{"abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"}
+	_, rS := Day2part2(lines)
+	var expected = "fgij"
+	if rS != expected {
+		t.Fatalf("Expected %s, got %s", expected, rS)
 	}
 }
